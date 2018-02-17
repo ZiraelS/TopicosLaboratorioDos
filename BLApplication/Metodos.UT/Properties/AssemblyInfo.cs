a8ceb02f243e75860cf65885bae04744f4769d2c
laboratorio <laboratorio@LAB9-07>
2018-02-17 14:56:49 -0600

    1er Commit. Creacion de la clase WCF "BLApplication" y Unit Testing "Metodos.UT".
    Estudiantes Presentes:
    Daniel Lepiz
    Kevin Mora

---
+++ BLApplication/Metodos.UT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Metodos.UT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Metodos.UT")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d202264-d2d7-49cc-b0e3-99e9558a8b65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
